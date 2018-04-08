c9c1bfe1561ffa60663dcd4591d8673e61ebe4fb
Zelion <savok130896@mail.ru>
2018-04-08 20:00:51 +0300

    remove useless libraries

--- diplom/wsgi.py
+++ diplom/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 import sys
 
-path = '/home/Zelion/diplom_tickets/diplom'
+path = '/home/Zelion/diplom_tickets'
 
 if path not in sys.path:
     sys.path.append(path)
