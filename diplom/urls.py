e4118f1d39b4445f5c1654523c0813d0b64d3ac0
Zelion <savok130896@mail.ru>
2018-04-08 20:27:07 +0300

    remove useless libraries

--- diplom/urls.py
+++ diplom/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^admin/$', admin.site.urls),
-    url(r'^$', include("mainApp.urls")),
-    url(r'^tasks/$', include('tasks.urls'))
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include("mainApp.urls")),
+    url(r'^tasks/', include('tasks.urls'))
 ]
