77e0ed93b04ade4ebb1b1120f57a0a1c88e1c47c
Zelion <savok130896@mail.ru>
2018-04-08 20:25:45 +0300

    remove useless libraries

--- diplom/urls.py
+++ diplom/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import url, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("mainApp.urls")),
-    path('tasks/', include('tasks.urls'))
+    url(r'^admin/$', admin.site.urls),
+    url(r'^$', include("mainApp.urls")),
+    url(r'^tasks/$', include('tasks.urls'))
 ]
