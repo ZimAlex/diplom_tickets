32bd58948a794776fc31ca5810c66e9eba3653f5
Zelion <savok130896@mail.ru>
2018-04-08 20:01:43 +0300

    remove useless libraries

--- diplom/wsgi.py
+++ diplom/wsgi.py
@@ -16,6 +16,6 @@ if path not in sys.path:
     sys.path.append(path)
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "/diplom/diplom.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "diplom.settings")
 
 application = get_wsgi_application()
