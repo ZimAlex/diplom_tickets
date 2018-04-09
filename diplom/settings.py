8d9bb3ceab301ff930b0a4dbc3f3779553eaac65
Zelion <savok130896@mail.ru>
2018-04-09 20:53:32 +0300

    remove useless libraries

--- diplom/settings.py
+++ diplom/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'diplom.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/home/Zelion/diplom_tickets/sqlite3.db'
     }
+
 }
 
 
