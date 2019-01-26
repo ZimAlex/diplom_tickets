9d87b6f950f1b79f778bb7c577cf89226da38ae0
Alex <savok130896@mail.ru>
2019-01-26 14:36:12 +0300

    Made task generation mostly from file

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -4,10 +4,13 @@ from django.views.generic.base import RedirectView, TemplateView
 from django.contrib import admin
 from django.urls import path, include
 
+app_name = 'mainApp'
+
 urlpatterns = [
-    url(r'^$', views.signup, name='signup'),
+    url(r'^$', views.signup_view, name='signup'),
+    url(r'login*$',views.login_view,name ='login'),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon/favicon.ico')),
-    path('template/login.html', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls'))
+    # path('template/login.html', TemplateView.as_view(template_name='login.html'), name='login'),
+    # path('admin/', admin.site.urls),
+    # path('accounts/', include('django.contrib.auth.urls'))
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
-def signup(request):
+def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -17,8 +17,17 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'mainApp/signup.html', {'form': form})
 
-
-
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #log in user
+            user = form.get_user()
+            login(request, user)
+            return redirect('tasks/generate')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'mainApp/login.html',{'form':form})
 
 
 # Create your views here.
